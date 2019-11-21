67b6a75a571ac4e9f419360c7a7c4e69d89417e2
KrisAnTis-Group <Krisantis@mail.ru>
2019-11-21 09:03:07 -0800

    categorical_crossentropy

--- first_vine_white.py
+++ first_vine_white.py
@@ -8,6 +8,15 @@ dataset = np.loadtxt("/home/krisantis/Desktop/Vine/red/winequality-red.txt", del
 X = dataset[:,0:-2]
 X=np.asarray(X).astype('float32')
 Y = dataset[:,-1:]
+Y=np.asarray(Y).astype('int')
+
+def to_one_hot(labels, demension=10):
+    results = np.zeros((len(labels),demension))
+    for i, label in enumerate(labels):
+        results[i,label]=1
+    return results
+
+Y = to_one_hot(Y)
 
 
 #Перемешивание вариантов
@@ -26,13 +35,13 @@ from keras import models
 from keras import layers
 
 model = models.Sequential()
-model.add(layers.Dense(64,activation='relu',input_shape=(X.shape[1],)))
-model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(32,activation='relu',input_shape=(X.shape[1],)))
 model.add(layers.Dense(32,activation='relu'))
-model.add(layers.Dense(16,activation='relu'))
-model.add(layers.Dense(1))
+#model.add(layers.Dense(32,activation='relu'))
+#model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(10,activation='softmax'))
 
-model.compile(optimizer='rmsprop',loss='mse',metrics=['acc'])
+model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
 
 history = model.fit(X, Y, epochs=32, batch_size=64, validation_split=0.3)
 #model.save_weights('Dense_model.h5')
@@ -41,8 +50,8 @@ history = model.fit(X, Y, epochs=32, batch_size=64, validation_split=0.3)
 
 import matplotlib.pyplot as plt
 
-acc=history.history['acc']
-val_acc = history.history['val_acc']
+acc=history.history['accuracy']
+val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
